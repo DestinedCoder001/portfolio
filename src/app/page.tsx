72aa1b67882c8d416af3285e50b8af804512fe94
DestinedCoder001 <149783390+DestinedCoder001@users.noreply.github.com>
2024-04-18 10:22:45 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import Projects from "@/components/Projects";
 import Skills from "@/components/Skills";
 
 export default function Home() {
+  // Triggering redeployment with this commit
   return (
     <div className="padding overflow-hidden">
       <Hero />
